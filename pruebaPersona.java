908c3b6e85dde9613875f1729b487d9582c427d6
manny1607 <unammannydsma@gmail.com>
2017-06-14 23:22:38 -0500

    Create pruebaPersona.java

--- pruebaPersona.java
+++ pruebaPersona.java
@@ -4,5 +4,6 @@ public class pruebaPersona
 		
 		Persona p1 = new Persona ();
 		Persona p2= new Persona ("Juanito", 22, 70);
+		p1.hablar("Hi friends!"); //prueba este codigo pero antes debes de poner los getters y setters en la clase Persona
 	}
 }
